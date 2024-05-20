b0b844e22897cc31024f418817cd4f531ad88acf
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-20 18:27:42 +0700

    commit 11 input int main dalam komposisi dengan varManusia dan delete varManusia

---
+++ Komposisi/Komposisi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "jantung.h"
+#include "manusia.h"
+
+int main()
+{
+    Manusia *varManusia = new Manusia("jino");
+    delete varManusia;
+    return 0;
+}
