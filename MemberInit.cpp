93e311ce017dc574c01e28817540f24f21f53cda
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-20 17:50:58 +0700

    commit 2 input public dalam class mahasiswa

--- MemberInit.cpp
+++ MemberInit.cpp
@@ -8,3 +8,17 @@ private:
     const int id;
     string nama;
     float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai)
+    { // Member Initialization List
+      //  Definisi
+    }
+
+    ~mahasiswa()
+    {
+        cout << "id	= " << id << endl;
+        cout << "Nama =" << nama << endl;
+        cout << "Nilai =" << nilai << endl;
+    }
+};
