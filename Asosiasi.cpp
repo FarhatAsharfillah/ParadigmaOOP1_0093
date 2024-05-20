734f472e264e63296f61457d557f584814fac6ec
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-20 19:15:47 +0700

    commit 14 input int main dengan vardokter dan varpasien dengan new pasien dan dokter, tambah pasien dan dokter, cetak pasien dan dokter, dan delete varpasien dan vardokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -34,7 +34,7 @@ public:
 };
 void pasien::cetakDokter() {
     cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
-    for (auto& a : daftar_dokter) { //auto digunakan dalam perulangan fpr untuk secara otomatis menentukan tipe data dari elemen yang diteras
+    for (auto& a : daftar_dokter) { //auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diteras
         cout << a->nama << "\n";
     }
     cout << endl;
@@ -49,4 +49,23 @@ void dokter::cetakPasien() {
         cout << a->nama << "\n";
     }
     cout << endl;
+}
+
+int main() {
+    dokter* varDokter1 = new dokter ("dr.Budi");
+    dokter* varDokter2 = new dokter ("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien();
+    varDokter2->tambahPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
 }
