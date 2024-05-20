f03e6049a78b4bf004eec78eceb14df95d71e4ef
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-20 18:26:33 +0700

    commit 10 input class manusia dengan public dan cout hidup

---
+++ Komposisi/manusia.h
@@ -0,0 +1,20 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class Manusia
+{
+public:
+    string name;
+    jantung varJantung;
+
+    Manusia(string pName) : name(pName)
+    {
+        cout << name << "hidup\n";
+    }
+    ~Manusia()
+    {
+        cout << name << "hidup\n";
+    }
+};
+#endif
